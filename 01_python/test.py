bc69adb01a0729db296e2c22dac0f4e2a223c06c
rahul8879 <rtiwarirahul123@gmail.com>
2026-04-23 06:24:12 +0530

    added the input examples

--- 01_python/test.py
+++ 01_python/test.py
@@ -46,20 +46,9 @@ x + y_new
 
 # list of keywords ??
 import keyword
-# print(keyword.kwlist)
+print(keyword.kwlist)
 # ['False', 'None', 'True', 'and', 'as', 'assert', 
 #  'async', 'await', 'break', 'class', 'continue', 'def', 'del',
 #    'elif', 'else', 'except', 'finally', 'for', 'from', 
 #    'global', 'if', 'import', 'in', 'is', 'lambda', 
 #  'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
-
-
-# int --> str
-# str -->int
-
-# input from user ?? condtion??
-age = int(input("Enter your age"))
-print("type of age",type(age))
-
-# next 5 year ??
-print(age + 5)
