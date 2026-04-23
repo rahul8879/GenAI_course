14c644ca4a19972c369bebafbeba2d4080f66229
rahul8879 <rtiwarirahul123@gmail.com>
2026-04-23 06:08:27 +0530

    added day 01 basic code

---
+++ 01_python/day_1.py
@@ -0,0 +1,55 @@
+# x = True
+# y = 5
+# z = x +y
+# print(z)
+# print(type(x))
+
+# types of variable 
+# integer
+# float
+# string
+# Boolean???
+
+# operation ??
+# 2 number(intger or float) ---> add, substraction, multiplcation 
+
+# eg 1;
+x = 3 # integer
+y = "5" # string
+
+# print("Type of x is : ",type(x))
+# print("Type of y is : ",type(y))
+
+# type casting??
+y_new = int(y)
+# print("type of y_new",type(y_new))
+
+x + y_new
+# z = x + y
+# print(z)
+
+# when you are doing any operation. - datatypes should match ???
+
+# x = "Rahul"
+# y = "test"
+# print(x+y)
+
+# rules for declaring variable name ??
+# 1. your variables cant start with digit
+# 2 your variable name cant start with specail char/symbol
+# 3. reserved keywords ---> you cant use them as a variable names
+# age_1 = 24
+# print(age_1)
+
+# @age = 24
+# print(age_1)
+
+# list of keywords ??
+import keyword
+print(keyword.kwlist)
+# ['False', 'None', 'True', 'and', 'as', 'assert', 
+#  'async', 'await', 'break', 'class', 'continue', 'def', 'del',
+#    'elif', 'else', 'except', 'finally', 'for', 'from', 
+#    'global', 'if', 'import', 'in', 'is', 'lambda', 
+#  'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+
